794b22bfedd1d95e9ed294dea4f7e7959987f59d
Bernardo Heynemann <bernardo@corp.globo.com>
2010-03-20 14:29:53 -0300

    available plugins

--- src/widgets-core.js
+++ src/widgets-core.js
@@ -1,35 +1,60 @@
+var errorTemplate = '<div style="padding: 0pt 0.7em;" class="ui-state-error ui-corner-all">';
+errorTemplate += '    <p><span style="float: left; margin-right: 0.3em;" class="ui-icon ui-icon-alert"></span>';
+errorTemplate += '    <strong>Alerta:</strong> {{message}}</p>';
+errorTemplate += '</div>';
+
 jQuery.widgets = {
     init: function(options){
         _self = this;
+        $('.errors').hide();
 
-        $("a.widget").click(function(){
-            var $a = $(this);
-            var url = $a.attr('href');
+        _self.bindAvailableWidgets(options.availableWidgets);
+    },
+    bindAvailableWidgets: function(availableWidgets){
+        $.each(availableWidgets, function(item){
+            var widget = this;
 
-            _self.loadWidget(url);
+            var $widget = $('<li><a href="' + widget.url + '" alt="' + widget.description + '" class="widget">' + widget.name + '</a></li>');
+            $('div.available-widgets').find('div.widgets').find('ul').append($widget);
 
-            return false;
+            $("a.widget", $widget).click(function(){
+                $('.errors').hide();
+                var $a = $(this);
+                var url = $a.attr('href');
+
+                _self.loadWidget(url);
+
+                return false;
+            });
+
         });
     },
     loadWidget: function(widgetName){
         var htmlUrl = 'widgets/' + widgetName + '.html';
         var jsUrl = 'widgets/' + widgetName + '.js';
 
-        $.get(htmlUrl, {}, function(data){
-            var $container = $('<div class="widget"></div>');
+        $.ajax({
+            url: htmlUrl,
+            success: function(data){
+                var $container = $('<div class="widget"></div>');
 
-            $container.append(data);
+                $container.append(data);
 
-            $('#widgets-area').append($container);
+                $('#widgets-area').append($container);
 
-            $.ajax({
-                url: jsUrl,
-                dataType: 'script',
-                success: function(data){
-                    var widgetObject = $[widgetName];
-                    widgetObject.add($container);
-                }
-            });
+                $.ajax({
+                    url: jsUrl,
+                    dataType: 'script',
+                    success: function(data){
+                        var widgetObject = $[widgetName];
+                        widgetObject.add($container);
+                    }
+                });
+            },
+            error: function(xmlHttpRequest, message, exception){
+                $('.errors').html(errorTemplate.replace('{{message}}', 'O widget n&atilde;o p&ocirc;de ser inserido com sucesso'));
+                $('.errors').show();
+            }
         });
     }
 };
