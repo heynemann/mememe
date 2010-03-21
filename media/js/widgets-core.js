257591adfcb3a9b41b7e0874da0eb2c541e2349f
Lincoln de Sousa <lincoln@minaslivre.org>
2010-03-21 02:00:17 -0300

    Heavy layout improvements and starting a refactory in widgets-core.
    This is currently broken to load all plugins at the same time. I am
    sharing the problem with others that are contributting to this
    program =P

--- media/js/widgets-core.js
+++ media/js/widgets-core.js
@@ -1,7 +1,24 @@
-var errorTemplate = '<div style="padding: 0pt 0.7em;" class="ui-state-error ui-corner-all">';
-errorTemplate += '    <p><span style="float: left; margin-right: 0.3em;" class="ui-icon ui-icon-alert"></span>';
-errorTemplate += '    <strong>Alerta:</strong> {{message}}</p>';
-errorTemplate += '</div>';
+/*
+ * widgets-core.js - this file is part of the 'mememe' project
+ *
+ * Copyright (C) 2010  Bernardo Heynemann <bernardo@corp.globo.com>
+ * Copyright (C) 2010  Gabriel Falcão <gabriel@nacaolivre.org>
+ * Copyright (C) 2010  Lincoln de Sousa <lincoln@comum.org>
+ * Copyright (C) 2010  Thiago Silva <tsilva@sourcecraft.info>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 var geoIp = {
     "query":{
@@ -28,99 +45,85 @@ var geoIp = {
             }
         }
     }
-}
+};
 
 //Para obter mais informacões geográficas
 //http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20google.geocoding%20where%20q%3D%22-23.5333%2C-46.6167%22&format=json&diagnostics=false&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=cbfunc
 
 jQuery.widgets = {
-    tabCounter:0,
-    init: function(options){
-        _self = this;
-        ip = options.ip;
 
-        $('.errors').hide();
-
-        $('#widgets-area').tabs({
-            tabTemplate: '<li><a href="#{href}" style="float:left;">#{label}</a><span id="close-#{href}" class="ui-icon ui-icon-close" style="float:left; margin: 10px 10px 0px -12px;">Remove Tab</span></li>',
-            add: function(event, ui) {
-                $(ui.panel).append('<div>Loading...</p>');
-            }
-        });
-
-        _self.bindAvailableWidgets(options.availableWidgets);
+    init: function (options)
+    {
+        this.bindAvailableWidgets(options.availableWidgets);
     },
-    bindAvailableWidgets: function(availableWidgets){
+
+    /** Iterates over all available widgets found, build their html,
+     * their js and call the main plugin function that fills the widget
+     * content. */
+    bindAvailableWidgets: function (availableWidgets)
+    {
+        /* This is the html that a widget must have. To avoid repeating it
+         * or writting a lots of jquery expressions, we're putting it here.
+         * The {{ title }} `var' will be replaced in this function, the
+         * content div will be filled by the loadWidget() method
+         * assyncronously. */
+        var widgetTemplate =
+            '<li>' +
+            '  <div class="widget">' +
+            '    <div class="header">' +
+            '      <strong>{{ title }}</strong>' +
+            '      <ul class="controls">' +
+            '        <li class="stick"><a href="javascript:;">stick</a></li>' +
+            '        <li class="close"><a href="javascript:;">close</a></li>' +
+            '      </ul>' +
+            '      <br class="clear" />' +
+            '    </div>' +
+            '    <div class="content"></div>' +
+            '  </div>' +
+            '</li>';
+
+        /* Preserving the `this' var to use it in another context. */
         var _self = this;
-        $.each(availableWidgets, function(item){
+
+        $.each(availableWidgets, function (item)
+        {
             var widget = this;
 
-            var $widget = $('<li>');
-            var $link = $('<a>');
-            $link.attr('js_url', widget.js_url)
-                .attr('html_url', widget.html_url)
-                .attr('href', 'javascript:;')
-                .attr('title', widget.description)
-                .attr('slug', widget.slug)
-                .addClass('widget')
-                .html(widget.name)
-                .appendTo($widget);
-
-            $('div.available-widgets').find('div.widgets').find('ul').append($widget);
-
-            $("a.widget", $widget).click(function(){
-                $('.errors').hide();
-                var $a = $(this);
-                _self.loadWidget($a);
-                return false;
-            });
-
+            /* Time to make the widget template become a jquery object,
+             * replacing the title var and then calling the load widget
+             * to fill the content. */
+            var widgetHtml = widgetTemplate.replace('{{ title }}', widget.name);
+            var $widget = $(widgetHtml);
+            $widget.appendTo($('#widgetsArea'));
+            _self.loadWidget($widget, widget);
         });
     },
-    loadWidget: function(link){
+
+    loadWidget: function (jElement, widgetObj) {
         var _self = this;
 
-        var htmlUrl = link.attr('html_url');
-        var jsUrl = link.attr('js_url');
-
+        /* Loading the html template of the plugin. */
         $.ajax({
-            url: htmlUrl,
-            success: function(data){
-                $tabs = $('#widgets-area');
-                var $container = $('<div class="widget"></div>');
-                $container.append(data);
+            url: widgetObj.html_url,
+            success: function (data) {
+                var $container = $('div.content', jElement).html(data);
 
-                var key = '#tabs-' + _self.tabCounter;
-                $tabs.tabs('add', key, link.html());
-                $tabs.tabs('select', _self.tabCounter);
-
-                _self.tabCounter++;
-
-                $(key).html($container);
-
-                $('span.ui-icon.ui-icon-close').unbind('click').bind('click', function(){
-                    var button = $(this);
-                    var tabIndex = $('li',$tabs).index(button.parent());
-
-                    $tabs.tabs('remove', tabIndex);
-                    _self.tabCounter--;
-                });
-
+                /* The plugin html file was loaded success full, it is time
+                 * to load its javascript file and execute the plugin
+                 * function inside of it. */
                 $.ajax({
-                    url: jsUrl,
+                    url: widgetObj.js_url,
                     dataType: 'script',
-                    success: function(data){
-                        var widgetObject = $[link.attr('slug')];
-                        widgetObject.add($container, geoIp);
-                    },
-                    error: function(xmlHttp, message, exception){
-                        var tabIndex = $('li',$tabs).index($container);
-                        $tabs.tabs('remove', tabIndex);
+                    success: function (data) {
+                        var plugin = $[widgetObj.slug];
+                        plugin.add($container, geoIp);
                     }
                 });
             },
-            error: function(xmlHttpRequest, message, exception){
-                $('.errors').html(errorTemplate.replace('{{message}}', 'O widget n&atilde;o p&ocirc;de ser inserido com sucesso'));
+
+            error: function (xmlHttpRequest, message, exception)
+            {
+                $('.errors').html('O widget n&atilde;o p&ocirc;de ser inserido com sucesso');
                 $('.errors').show();
             }
         });
