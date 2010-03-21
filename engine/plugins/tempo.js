35d8c43a69bbd87fb4472c138f1e08f462cef35b
Gabriel Falcão <gabriel@nacaolivre.org>
2010-03-21 08:38:37 -0300

    refactoring tempo widget, to use jquery-yql

--- engine/plugins/tempo.js
+++ engine/plugins/tempo.js
@@ -1,16 +1,11 @@
-
 jQuery.tempo = {
     add:function(container, geoData) {
-        var url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.bylocation%20where%20location%3D'{{city}}'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=?"
-
-
-        var myIpUrl = url.replace('{{city}}', geoData.query.results.Response.City.replace("ã","a"));
-        
-        $.ajax({
-            url: myIpUrl,
-            dataType: 'jsonp',
-            success: function(data) {
-                x = data;
+        $.yql(
+            "SELECT * FROM weather.bylocation WHERE location=#{cidade}",
+            {
+                cidade: geoData.query.results.Response.City.replace("ã","a")
+            },
+            function (data){
                 var astronomy  = data.query.results.weather.rss.channel.astronomy; //sunrise, sunset
                 var atmosphere = data.query.results.weather.rss.channel.atmosphere; //humidity,pressure,rising,visibility
                 var cond       = data.query.results.weather.rss.channel.item.condition; //date,temp,text
@@ -19,7 +14,7 @@ jQuery.tempo = {
                 var dv =  '<div class="night" id="yw-forecast">'
                     + '<dl>'
                     + '<dt>Feels Like:</dt><dd>'+wind.chill+' °F</dd>'
-                    + '<dt>Barometer:</dt><dd style="position: relative;">'+atmosphere.pressure+' in and ' 
+                    + '<dt>Barometer:</dt><dd style="position: relative;">'+atmosphere.pressure+' in and '
                     + (atmosphere.rising=="0"?"falling":"rising")+'</dd>'
                     + '<dt>Humidity:</dt><dd>'+atmosphere.humidity+' %</dd>'
                     + '<dt>Visibility:</dt><dd>'+atmosphere.visibility+' mi</dd>'
@@ -38,8 +33,7 @@ jQuery.tempo = {
                     + '</div>';
 
                 $('#tempo', container).append(dv);
-
             }
-        });
+        );
     }
 }
